f438ee841e9485fc7e5386caa40e2689ea029833
nagasundharam <nagasundharam553@gmail.com>
2024-09-09 11:28:18 +0530

    express routers

--- expressjs/app.js
+++ expressjs/app.js
@@ -1,17 +1,30 @@
-const http = require('http');
+
 const express = require('express');
 
 const app = express();
 
-app.use((req,res,next)=> {
-    console.log('First middleware');
-    next();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded())
+
+app.get('/add-product',(req,res,next) => {
+    res.send('<h1>Add products</h1> <form action = "/store-product" method ="POST"><input type = "text" name = "title"/><input type ="submit" value ="Submit"/></form>')
+  
+    
 })
-app.use((req,res) => {
-    console.log('second middleware');
+app.post ('/store-product',(req,res,next) => {
+    console.log('Form data',req.body.title);
+    res.send('<b>Product Submitted</b>');
+  
+    
 })
 
-const server = http.createServer(app);
-server.listen(3000);
+app.use('/',(req,res,next)=> {
+    res.send('<h1>First<h1/>');
+    console.log("f irst middleware");
+    // next();
+})
 
 
+
+app.listen(3000);
